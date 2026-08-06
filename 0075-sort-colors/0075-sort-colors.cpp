8a3443c4005e50af8746f4a2550c6c5cf51679c8
Aranjay G-1 <153104775+aranjay-1@users.noreply.github.com>
2026-08-06 16:45:47 +0530

    Time: 0 ms (100%), Space: 11.6 MB (79.79%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        void sortColors(vector<int>& nums) {
+                int low{}, mid{}, high{static_cast<int>(nums.size()) - 1};
+                        while (mid <= high) {
+                                    if (nums[mid] == 0) {
+                                                    swap(nums[mid++], nums[low++]);
+                                                                } else if (nums[mid] == 1) {
+                                                                                mid++;
+                                                                                            } else {
+                                                                                                            swap(nums[mid], nums[high--]);
+                                                                                                                        }
+                                                                                                                                }
+                                                                                                                                    }
+                                                                                                                                    };
